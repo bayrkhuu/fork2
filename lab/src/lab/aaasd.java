eb84e094ca13a0013127aa2b329af61100460ac9
Admin <Admin@DESKTOP-QLU378L>
2023-04-19 12:17:51 +0800

    asdasd

--- lab/src/lab/aaasd.java
+++ lab/src/lab/aaasd.java
@@ -4,7 +4,7 @@ public class aaasd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aaaaa");
+		System.out.println("aaaasdasdasda");
 	}
 
 }
